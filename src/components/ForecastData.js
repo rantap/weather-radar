958eb543401c70abed5bab8bae555ac3611007b8
Petri <peewter@gmail.com>
2023-03-18 13:50:04 +0200

    Implement loading state

--- src/components/ForecastData.js
+++ src/components/ForecastData.js
@@ -3,42 +3,48 @@ import ForecastCard from './ForecastCard';
 
 const ForecastData = ({ location }) => {
   const [forecastData, setForecastData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const urlBase = 'https://api.openweathermap.org/data/2.5/forecast?';
   const apiKey = process.env.REACT_APP_API_KEY;
   useEffect(() => {
     const fetchForecastData = async (location) => {
       try {
+        setLoading(true);
         const res = await fetch(
           urlBase +
             `lat=${location.latitude}&lon=${location.longitude}&units=metric&appid=` +
             apiKey,
         );
         const forecastJsonObject = await res.json();
-        const newState = forecastJsonObject.list.slice(1, 6);
-        setForecastData(newState);
-        console.log(forecastData);
+        const newForecast = forecastJsonObject.list.slice(1, 6);
+        setForecastData(newForecast);
+        setLoading(false);
       } catch (err) {
+        setLoading(false);
         console.log(err);
       }
     };
     fetchForecastData(location).catch(console.error);
   }, []);
-  console.log(forecastData);
 
   return (
     <div className='forecast-flex'>
-      {forecastData.map((data, index) => (
-        <ForecastCard
-          datetime={data.dt}
-          icon={data.weather[0].icon}
-          temperature={data.main.temp}
-          wind={data.wind.speed}
-          humidity={data.main.humidity}
-          precipitation={data[5]}
-          key={index}
-        />
-      ))}
+      {loading ? (
+        <p>Loading..</p>
+      ) : (
+        forecastData.map((data, index) => (
+          <ForecastCard
+            datetime={data.dt}
+            icon={data.weather[0].icon}
+            temperature={data.main.temp}
+            wind={data.wind.speed}
+            humidity={data.main.humidity}
+            precipitation={data.pop}
+            key={index}
+          />
+        ))
+      )}
     </div>
   );
 };
--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -3,12 +3,15 @@ import CurrentWeatherCard from './CurrentWeatherCard';
 
 const WeatherData = ({ location }) => {
   const [weatherData, setWeatherData] = useState({});
+  const [loading, setLoading] = useState(false);
 
   const urlBase = 'https://api.openweathermap.org/data/2.5/weather?';
   const apiKey = process.env.REACT_APP_API_KEY;
+  
   useEffect(() => {
     const fetchWeatherData = async (location) => {
       try {
+        setLoading(true);
         const res = await fetch(
           urlBase +
             `lat=${location.latitude}&lon=${location.longitude}&units=metric&appid=` +
@@ -22,10 +25,11 @@ const WeatherData = ({ location }) => {
           wind: weatherJsonObject.wind.speed,
           humidity: weatherJsonObject.main.humidity,
           icon: weatherJsonObject.weather[0].icon,
-          // precipitation: weatherJsonObject.snow['1h'],
+          /* precipitation: weatherJsonObject.snow['1h'], */
         });
-        console.log(weatherJsonObject);
+        setLoading(false);
       } catch (err) {
+        setLoading(false);
         console.log(err);
       }
     };
@@ -34,15 +38,19 @@ const WeatherData = ({ location }) => {
 
   return (
     <div>
-      <CurrentWeatherCard
-        city={weatherData.city}
-        description={weatherData.description}
-        temperature={weatherData.temperature}
-        wind={weatherData.wind}
-        humidity={weatherData.humidity}
-        icon={weatherData.icon}
-        // precipitation={weatherData.precipitation}
-      />
+      {loading ? (
+        <p>Loading..</p>
+      ) : (
+        <CurrentWeatherCard
+          city={weatherData.city}
+          description={weatherData.description}
+          temperature={weatherData.temperature}
+          wind={weatherData.wind}
+          humidity={weatherData.humidity}
+          icon={weatherData.icon}
+          /* precipitation={weatherData.precipitation} */
+        />
+      )}
     </div>
   );
 };
