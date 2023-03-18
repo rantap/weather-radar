d3bb67828248e79a19ee67cdbcd1906ba66d51a8
Petri <peewter@gmail.com>
2023-03-18 17:58:49 +0200

    Add comments

--- src/components/ForecastData.js
+++ src/components/ForecastData.js
@@ -4,9 +4,10 @@ import ForecastCard from './ForecastCard';
 const ForecastData = ({ location }) => {
   const [forecastData, setForecastData] = useState([]);
   const [loading, setLoading] = useState(false);
-
   const urlBase = 'https://api.openweathermap.org/data/2.5/forecast?';
   const apiKey = process.env.REACT_APP_API_KEY;
+
+  // Fetch forecast weather data from the API
   useEffect(() => {
     const fetchForecastData = async (location) => {
       try {
@@ -17,6 +18,7 @@ const ForecastData = ({ location }) => {
             apiKey,
         );
         const forecastJsonObject = await res.json();
+        // Slice the needed forecasts from the JSON object
         const newForecast = forecastJsonObject.list.slice(1, 6);
         setForecastData(newForecast);
         setLoading(false);
@@ -40,7 +42,13 @@ const ForecastData = ({ location }) => {
             temperature={data.main.temp}
             wind={data.wind.speed}
             humidity={data.main.humidity}
-            precipitation={data.pop}
+            // Check the correct key for precipitation
+            // If not available set to 0
+            precipitation={
+              (data.snow && data.snow['3h']) ||
+              (data.rain && data.rain['3h']) ||
+              0
+            }
             key={index}
           />
         ))
