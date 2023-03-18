0dbe7e395ac5542d2b41b6d3f1c5cbd8d3671c20
Petri <peewter@gmail.com>
2023-03-18 13:53:56 +0200

    Implement datetime & description formatting and conditional rendering for description and icon

--- src/components/CurrentWeatherCard.js
+++ src/components/CurrentWeatherCard.js
@@ -1,29 +1,53 @@
+import * as moment from 'moment';
+import 'moment/locale/en-gb';
+
 const CurrentWeatherCard = ({
   city,
   description,
   temperature,
   wind,
   humidity,
-  /* precipitation, */
+  precipitation,
   icon,
 }) => {
+  const date = moment().format('MMMM Do');
+  const time = moment().format('LT');
+
   return (
     <div className='datacard'>
-      <div>
-        <h2 className='datacard-city'>{city}</h2>
-        <p className='datacard-description'>{description}</p>
+      <div className='datacard-upper'>
+        <div className='datacard-city-description'>
+          <h2 className='datacard-city'>{city}</h2>
+          <p className='datacard-description'>
+            {description &&
+              description.charAt(0).toUpperCase() + description.slice(1)}
+          </p>
+        </div>
+        <div className='icon-container'>
+          <img
+            src={icon && 'http://openweathermap.org/img/wn/' + icon + '@2x.png'}
+          />
+          <div className='side-content-center'>
+            <p className='datacard-temperature'>{temperature} °C</p>
+          </div>
+        </div>
       </div>
-      <div>
-        <img src={'http://openweathermap.org/img/wn/' + icon + '@2x.png'} />
-        <p className='datacard-temperature'>{temperature} °C</p>
-      </div>
-      <div className='datacard-additional'>
-        <p>Wind: {wind} m/s</p>
-        <p>Humidity: {humidity} %</p>
-        {/* <p>Precipitation (1 h): {precipitation} mm</p> */}
+      <div className='datacard-lower'>
+        <div className='datacard-datetime'>
+          <br />
+          <p className='datacard-date'>{date}</p>
+          <p className='datacard-time'>{time}</p>
+        </div>
+        <div className='datacard-additional'>
+          <p>Wind: {wind} m/s</p>
+          <p>Humidity: {humidity} %</p>
+          <p>Precipitation (1 h): {Math.round(precipitation)} mm</p>
+        </div>
       </div>
     </div>
   );
 };
 
 export default CurrentWeatherCard;
+
+// description.charAt(0).toUpperCase() + description.slice(1)
