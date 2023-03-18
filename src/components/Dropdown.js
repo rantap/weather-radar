bcc57d9398a376a86c41c38820d731d70061e6b6
Petri <peewter@gmail.com>
2023-03-18 13:51:49 +0200

    Style the dropdown list button

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,7 +4,28 @@ import Select from '@mui/material/Select';
 const Dropdown = ({ handleChange, selected }) => {
   return (
     <div>
-      <Select value={selected} onChange={handleChange} displayEmpty>
+      <Select
+        fullWidth
+        value={selected}
+        onChange={handleChange}
+        displayEmpty
+        sx={{
+          fontFamily: 'Arial',
+          fontSize: '13px',
+          backgroundColor: 'white',
+          textAlign: 'left',
+          paddingLeft: '0.3rem',
+          width: '100%',
+          height: '3rem',
+          color: '#262626',
+          '& .MuiSvgIcon-root': {
+            color: 'black',
+          },
+          '.MuiOutlinedInput-notchedOutline': {
+            borderColor: '#e6e6e6',
+          },
+        }}
+      >
         <MenuItem value={'All'}>Kaikki kaupungit</MenuItem>
         <MenuItem value={'Tampere'}>Tampere</MenuItem>
         <MenuItem value={'Jyväskylä'}>Jyväskylä</MenuItem>
