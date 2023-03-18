62236802902d1b824978f0e0afa7a161aab608f5
Petri <peewter@gmail.com>
2023-03-18 13:51:14 +0200

    Implement datetime formatting

--- src/components/ForecastCard.js
+++ src/components/ForecastCard.js
@@ -1,3 +1,5 @@
+import * as moment from 'moment';
+import 'moment/locale/en-gb';
 const ForecastCard = ({
   datetime,
   temperature,
@@ -6,17 +8,18 @@ const ForecastCard = ({
   precipitation,
   icon,
 }) => {
+  const time = moment.utc(datetime * 1000).format('LT');
   return (
     <div className='forecast-card'>
       <div>
-        <p className='forecast-time'>12:00</p>
+        <p className='forecast-time'>{time}</p>
         <img src={'http://openweathermap.org/img/wn/' + icon + '.png'} />
         <p className='forecast-temperature'>{Math.round(temperature)}°C</p>
       </div>
       <div className='forecast-additional'>
         <p>{wind} m/s</p>
         <p>{humidity} %</p>
-        <p>{precipitation} mm</p>
+        <p>{Math.round(precipitation)} mm</p>
       </div>
     </div>
   );
